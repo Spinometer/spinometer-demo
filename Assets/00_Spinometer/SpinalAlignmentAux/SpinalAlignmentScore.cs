8d214f8925fa4588160f0835aa7cdac2aabf88ca
Tomoaki Hayasaka <hayasaka@malmodeler.com>
2024-09-30 04:23:13 +0900

    refactor:  add SpinalAlignmentScore.

---
+++ Assets/00_Spinometer/SpinalAlignmentAux/SpinalAlignmentScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using GetBack.Spinometer.SpinalAlignmentCore;
+using Newtonsoft.Json;
+
+namespace GetBack.Spinometer.SpinalAlignmentAux
+{
+  public class SpinalAlignmentScore
+  {
+    public Dictionary<SpinalAlignment.RelativeAngleId, float> normalizedRelativeAngles = new();
+    public Dictionary<SpinalAlignment.RelativeAngleId, float> scores = new();
+
+    public SpinalAlignmentScore Clone()
+    {
+      var serialized = JsonConvert.SerializeObject(this);
+      var cloned = JsonConvert.DeserializeObject<SpinalAlignmentScore>(serialized);
+      return cloned;
+    }
+  }
+}
---
+++ Assets/00_Spinometer/SpinalAlignmentAux/SpinalAlignmentScoreCalculator.cs
@@ -0,0 +1,51 @@
+﻿using GetBack.Spinometer.SpinalAlignmentCore;
+using UnityEngine;
+
+namespace GetBack.Spinometer.SpinalAlignmentAux
+{
+  public class SpinalAlignmentScoreCalculator
+  {
+    public static void CalculateScore(SpinalAlignment alignment_in, SpinalAlignmentScore score_out)
+    {
+      CalculateNormalizedAngles(alignment_in, score_out);
+      CalculateScore_(score_out);
+    }
+
+    private static float NormalizedAngle(float angle, float normalCenter, float normalHalfWidth)
+    {
+      return (angle - normalCenter) / normalHalfWidth;
+    }
+
+    private static void CalculateNormalizedAngles(SpinalAlignment alignmentIn, SpinalAlignmentScore scoreOut)
+    {
+      void calc(float normalCenter, float normalHalfWidth, SpinalAlignment.RelativeAngleId id)
+      {
+        if (!alignmentIn.relativeAngles.ContainsKey(id))
+          return;
+        var angle = alignmentIn.relativeAngles[id];
+        scoreOut.normalizedRelativeAngles[id] = NormalizedAngle(angle, normalCenter, normalHalfWidth);
+      }
+
+      calc(30f, 5f,
+           SpinalAlignment.RelativeAngleId.C2_C7_vert_new);
+      calc(40f, 10f,
+           SpinalAlignment.RelativeAngleId.C7_T3_vert_new);
+      calc(150f, 8f,
+           SpinalAlignment.RelativeAngleId.C7_T3_T8);
+      calc(155f, 1f,
+           SpinalAlignment.RelativeAngleId.T3_T8_T12);
+      calc(177.7f, 2.5f,
+           SpinalAlignment.RelativeAngleId.T8_T12_L3);
+      calc(172.5f, 1.5f,
+           SpinalAlignment.RelativeAngleId.T12_L3_S);
+    }
+
+    private static void CalculateScore_(SpinalAlignmentScore scoreOut)
+    {
+      foreach (var kv in scoreOut.normalizedRelativeAngles) {
+        var score = -(Mathf.Abs(kv.Value) - 1.0f);
+        scoreOut.scores[kv.Key] = score;
+      }
+    }
+  }
+}
